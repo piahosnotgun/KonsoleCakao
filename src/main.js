c01779f260989f160f16f78b690d30f8ae4ca707
Del-Los-Leones <dellosleones@pusan.ac.kr>
2021-06-01 14:47:23 +0000

    typescript 컴파일 결과를 .gitignore에 포함

--- src/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const konsolecakao_1 = require("./konsolecakao");
-const userinfo_1 = require("./userinfo");
-const info = new userinfo_1.UserInfo();
-console.log(info);
-const kc = new konsolecakao_1.KonsoleCakao(info);
-console.log(kc);
-kc.login().then();
