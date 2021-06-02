acdcad4b515aa0154073ccaf0475f996ed80532e
Del-Los-Leones <dellosleones@pusan.ac.kr>
2021-06-02 14:12:45 +0000

    메뉴 부모 클래스 구현 및 디스플레이의 부모 클래서의 사용 정의

--- src/cui/display/display.ts
+++ src/cui/display/display.ts
@@ -1,11 +1,32 @@
 import { TalkClient } from "node-kakao";
+import { Menu } from "menu/menu";
+import * as readline from "readline";
 
 export class Display {
 	private tc: TalkClient;
-	// menuName => callback
-	private menus = {};
+	private menus: Menu[];
 	constructor(tc: TalkClient){
 		this.tc = tc;
 	}
-	
+	public addMenu(menu: Menu){
+		this.menus.push(menu);
+	}
+	public showMenus(){
+		for(let num in this.menus){
+			let menu = this.menus[num];
+			if(typeof menu === undefined)
+				continue;
+			console.log(num + ". " + menu.getText());
+		}
+	}
+	public select(num: int){
+		if(num >= this.menus.length || num < 0){
+			console.log("잘못된 숫자입니다. 다시 입력해주세요.");
+		}
+		let menu = this.menus[num];
+		if(typeof menu === "undefined"){
+			return;
+		}
+		menu.onSelect();
+	}
 }
--- src/cui/menu/menu.ts
+++ src/cui/menu/menu.ts
@@ -1,4 +1,7 @@
 export abstract class Menu {
 	protected text: string;
 	public abstract onSelect();
+	public getText(){
+		return this.text;
+	}
 }
